62707dc59d35fb1f6aa765eb8d80cc69d13d567c
takeso10 <sotatake7716@gmail.com>
2023-01-20 15:49:13 +0900

    newprofileのところで躓いている、型が合わないっぽい

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import {useNavigate} from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
+
 function App() {
   const navigate = useNavigate()
+  const state = useLocation().state
 
   return (
     <div className="self-introduction">
       <h1>プロフィール</h1>
-      <h2>名前：</h2>
+      <h2>名前：{state}</h2>
       <p>年齢：</p>
       <p>趣味：</p>
       <p>専攻：</p>
--- my-app/src/Edit.tsx
+++ my-app/src/Edit.tsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './Edit.css';
 import {useNavigate} from 'react-router-dom'
 
 function Edit(){
+    const [inputName,setInputName]=useState('')
+    const [inputAge,setInputAge]=useState(0)
+    const [inputHobby,setInputHobby]=useState('')
+    const [inputMajor,setInputMajor]=useState('')
+    const [inputMessage,setInputMessage]=useState('')
     const navigate = useNavigate()
+    const [profile,setProfile] = useState<Profile>()
+    
+    type Profile = {
+        inputName:string;
+        inputAge:number;
+        inputHobby:string;
+        inputMajor:string;
+        inputMessage:string;
+    }
+
+    const handleSubmit=(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+
+        const newProfile : Profile = {
+            inputName:inputName,
+            inputAge:inputAge,
+            inputHobby:inputHobby,
+            inputMajor:inputMajor,
+            inputMessage:inputMessage,
+        }
+        setProfile(newProfile)
+        console.log(newProfile)
+    }
 
     return(
         <div className="self-introduction">
-            <form onSubmit={()=>{}}>
+            <form onSubmit={(e)=>{handleSubmit(e)}}>
                 <h1>プロフィール</h1>
                 <p>名前</p>
-                <input type='text' onChange={()=>{}} className='inputName'></input>
+                <input type='text' onChange={(e)=>{setInputName(e.target.value)}} className='inputName'></input>
                 <p>年齢</p>
-                <input type='text'onChange={()=>{}} className='inputAge'></input>
+                <input type='number'onChange={(e)=>{setInputAge(e.target.valueAsNumber)}} className='inputAge'></input>
                 <p>趣味</p>
-                <input type='text'onChange={()=>{}} className='inputHobby'></input>
+                <input type='text'onChange={(e)=>{setInputHobby(e.target.value)}} className='inputHobby'></input>
                 <p>専攻</p>
-                <input type='text'onChange={()=>{}} className='inputMajor'></input>
+                <input type='text'onChange={(e)=>{setInputMajor(e.target.value)}} className='inputMajor'></input>
                 <h2>ひとこと</h2>
-                <textarea onChange={()=>{}} className='inputMessage'></textarea>
-                <button type='submit' onClick={()=>{navigate('/')}}>更新</button>
+                <textarea onChange={(e)=>{setInputMessage(e.target.value)}} className='inputMessage'></textarea>
+                <button type='submit' onClick={()=>{navigate('/',{state:{profile}})}}>更新</button>
             </form>
         </div>
     )
